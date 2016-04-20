9c93554654aa8b9f9de6fca5b2a7c24d90e3b475
Edgar Ribeiro <edgar.marques.ribeiro@gmail.com>
2016-04-20 18:35:35 +0100

    improved request, track and init interface

--- elastic-event.js
+++ elastic-event.js
@@ -14,49 +14,95 @@
 })(this, function () {
   function ElasticEvent() {
     this.queue = [];
+    this.max = 256;
+    this.interval = 10000;
+    this.host = 'localhost:9200';
+    this.url = this.host;
   }
 
-  ElasticEvent.prototype.init = function (options) {
-    var self = this;
-    var unload;
-    var unloadInterval;
-
-    if (this.close) {
-      throw new Error('already initialized');
+  ElasticEvent.prototype.setupIntervalSend = function (options) {
+    if (this.intervalSend) {
+      throw new Error('already setup intervalSend');
     }
 
-    this.max = options.max || 256;
-    this.host = options.host || 'localhost:9200';
-    this.index = options.index || 'event';
-    this.type = options.type;
-    this.url = this.host + '/' + this.index;
+    this.interval = options.interval || this.interval;
+
+    this.intervalSend = setInterval(
+      this.send.bind(this, false),
+      this.interval
+    );
+  };
+
+  ElasticEvent.prototype.setupBeforeUnload = function () {
+    if (this.beforeunload) {
+      throw new Error('already setup beforeUnload');
+    }
+
+    // make sure it sends queued events on exit
+    this.beforeunload = this.send.bind(this, true);
+    window.addEventListener('beforeunload', this.beforeunload);
+  };
+
+  ElasticEvent.prototype.setup = function (options) {
+    this.setupIntervalSend(options);
+    this.setupBeforeUnload();
+  };
+
+  ElasticEvent.prototype.close = function () {
+    self.send();
+    if (this.intervalSend) {
+      clearInterval(this.intervalSend);
+      this.intervalSend = null;
+    }
+    if (this.beforeunload) {
+      window.removeEventListener('beforeunload', this.beforeunload);
+      this.beforeunload = null;
+    }
+  };
+
+  ElasticEvent.prototype.init = function (options) {
+    this.config(options);
+    this.setup(options);
+  };
+
+  ElasticEvent.prototype.config = function (options) {
+    this.max = options.max || this.max;
+    this.host = options.host || this.host;
+    this.index = options.index || this.index;
+    this.type = options.type || this.type;
+
+    this.url = this.host;
+
+    if (this.index) {
+      this.url += '/' + this.index;
+    }
     if (this.type) {
       this.url += '/' + this.type;
     }
-
-    unloadInterval = setInterval(
-      this.send.bind(this, false),
-      options.interval || 10000
-    );
-
-    // make sure it sends queued events on exit
-    unload = this.send.bind(this, true);
-
-    window.addEventListener('beforeunload', unload);
-
-    this.close = function () {
-      self.send();
-      self.close = null;
-      clearInterval(unloadInterval);
-      window.removeEventListener('beforeunload', unload);
-    };
   };
 
-  function request(url, data, async) {
+  function request(url, data, async, callback) {
     var xhr = new XMLHttpRequest();
+
+    if (typeof callback === 'function') {
+      xhr.ontimeout = xhr.onerror = xhr.onload = function () {
+        var err = null;
+        if (!xhr.status || xhr.status >= 400) {
+          err = new Error(xhr.statusText || 'request failed');
+        }
+        callback(err, xhr.responseText ? JSON.parse(xhr.responseText) : null,
+          xhr.status, xhr);
+      };
+    }
+
     xhr.open('POST', url, async);
-    xhr.setRequestHeader('Content-Type', 'text/plain');
-    xhr.send(data);
+
+    if (data) {
+      xhr.setRequestHeader('Content-Type', 'text/plain');
+      xhr.send(data);
+    } else {
+      xhr.send(null);
+    }
     return xhr;
   }
 
@@ -71,12 +117,67 @@
     );
   };
 
-  ElasticEvent.prototype.log = function (event, meta) {
+  ElasticEvent.prototype.search = function (query, callback) {
+    request(
+      this.url + '/_search',
+      query ? JSON.stringify(query) : null,
+      true,
+      callback
+    );
+  };
+
+  function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0;
+      var v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+  ElasticEvent.prototype.identify = function (userId) {
+    this.sessionId = this.sessionid || uuid();
+
+    if (userId && typeof userId !== 'string') {
+      throw new Error('invalid typeof userId');
+    }
+
+    if (userId) {
+      this.userId = userId;
+    }
+  };
+
+  ElasticEvent.prototype.baseEvent = function (event) {
+    var baseEvent = event || {};
+
+    if (this.userId) {
+      baseEvent.userId = this.userId;
+    }
+
+    if (this.sessionId) {
+      baseEvent.sessionId = this.sessionId;
+    }
+
+    baseEvent.timestamp = new Date().toISOString();
+
+    return baseEvent;
+  };
+
+  ElasticEvent.prototype.track = function (type, event) {
+    if (typeof type !== 'string') {
+      throw new Error('invalid typeof type');
+    }
+
+    this.add(this.baseEvent(event), {
+      type: type
+    });
+  };
+
+  ElasticEvent.prototype.add = function (event, meta) {
     var data;
     var info = meta || {};
     var index = {
       _index: info.index || this.index,
-      _type: info.type || this.type || 'log'
+      _type: info.type || this.type
     };
 
     if (typeof event !== 'object') {
@@ -84,7 +185,7 @@
     }
 
     if (info.id) {
-      index._id = meta.id;
+      index._id = info.id;
     }
 
     data = JSON.stringify({ index: index }) + '\n' +
